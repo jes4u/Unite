042658d1fdaa0b8a785cb82b606d9d0dbce05fc4
Jesse <jes4u@uw.edu>
2019-04-29 12:16:49 -0700

    Pulled from master, figuring out leaflet box

--- Client-Side/index.js
+++ Client-Side/index.js
@@ -25,11 +25,16 @@ $(document).ready(function() {
         sidebarTitleText: 'Header',
         sidebarMenuItems: {
             Items: [
-                { type: "link", name: "Link 1 (github.com)", href: "http://github.com", icon: "icon-local-carwash" },
-                { type: "link", name: "Link 2 (google.com)", href: "http://google.com", icon: "icon-cloudy" },
-                { type: "button", name: "Button 1", onclick: "alert('button 1 clicked !')", icon: "icon-potrait" },
-                { type: "button", name: "Button 2", onclick: "button2_click();", icon: "icon-local-dining" },
-                { type: "link", name: "Link 3 (stackoverflow.com)", href: 'http://stackoverflow.com', icon: "icon-bike" },
+                { 
+                    type: "checkbox",
+                    name: "GDP", 
+                    onclick: "createGDP();" 
+                }
+                ,{ type: "link", name: "Link 1 (github.com)", href: "http://github.com", icon: "icon-local-carwash" }
+                // { type: "link", name: "Link 2 (google.com)", href: "http://google.com", icon: "icon-cloudy" },
+                // { type: "button", name: "Button 1", onclick: "alert('button 1 clicked !')", icon: "icon-potrait" },
+                // { type: "button", name: "Button 2", onclick: "button2_click();", icon: "icon-local-dining" },
+                // { type: "link", name: "Link 3 (stackoverflow.com)", href: 'http://stackoverflow.com', icon: "icon-bike" },
             ]
         }
     });
@@ -42,8 +47,20 @@ $(document).ready(function() {
     }
     mymap.addControl(control);
 
+    
 });
 
 function button2_click(){
-        alert('button 2 clicked !!!');
+    alert('button 2 clicked !!!');
 }
+function createGDP() {
+    alert('hi');
+    let gdpDiv = document.createElement("div");
+    let gdpSlider = document.createElement("input");
+    gdpSlider.setAttribute("type", "range");
+    gdpSlider.setAttribute("max", "5");
+    gdpSlider.setAttribute("min", "0");
+    gdpDiv.appendChild(gdpSlider);
+
+}
+
