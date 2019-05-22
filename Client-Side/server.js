49e549e4de65eced5b4da4063e7328b3dab53ef0
unknown <chesdude@gmail.com>
2019-05-22 11:50:10 -0700

    nothing changed

--- Client-Side/server.js
+++ Client-Side/server.js
@@ -3,29 +3,29 @@ const path = require('path');
 const express = require('express');
 const app = express();    
 
-// app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, '')));
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// http.createServer(app).listen(8000, function(){
-//     console.log('Listening on port 8000');
-// });
+http.createServer(app).listen(8000, function(){
+    console.log('Listening on port 8000');
+});
 
-var fs = require('fs');
-fs.readFile('Data/GeoJSONFiles/citypoint.geojson', 'utf8', function readFileCallback(err, data){
-    if (err){
-        console.log(err);
-    } else {
-    obj = JSON.parse(data); //now it an object
-    //console.log(obj['features']);
-    for (var feature in obj['features']) {
-        console.log(feature['properties']);
-        if (feature.properties.NAME == null) {
-            delete feature;
-        }
-    }
-    json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
-}});
+// var fs = require('fs');
+// fs.readFile('Data/GeoJSONFiles/citypoint.geojson', 'utf8', function readFileCallback(err, data){
+//     if (err){
+//         console.log(err);
+//     } else {
+//     obj = JSON.parse(data); //now it an object
+//     //console.log(obj['features']);
+//     for (var feature in obj['features']) {
+//         console.log(feature['properties']);
+//         if (feature.properties.NAME == null) {
+//             delete feature;
+//         }
+//     }
+//     json = JSON.stringify(obj); //convert it back to json
+//     fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
+// }});
